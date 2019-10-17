d79b71a45c22afba6dde003c4a5a2340b1b6ffd1
Dan Church <dan.church@workiva.com>
2019-10-17 00:26:44 -0600

    Clean up code, hook up fix commands

--- testDir/test.dart
+++ testDir/test.dart
@@ -8,6 +8,29 @@ import 'package:w_sox/ui_core.dart';
 import 'package:meta/meta.dart';
 import 'package:path/path.dart';
 
-class Test {
+class M {
+  int a;
+  bool b;
+  Object c;
+  Function d;
+  String f;
 
+  void e() {}
+  void aa() {}
+}
+
+class Test {
+  Test() {
+    new M()
+      ..b = false
+      ..c = true
+      ..e()
+      ..aa()
+      // comment for d
+      ..d = () {
+
+      }
+      ..f = "double quotes"
+      ..a = 0;
+  }
 }
